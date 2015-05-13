e521965e564abe23a471f6e764d84c8e06db0308
GJMcGowan <gemcgow@gmail.com>
2015-05-13 17:07:02 +0100

    Adding failing protractor test

--- test/e2e/cohortDisplayFeature.js
+++ test/e2e/cohortDisplayFeature.js
@@ -4,4 +4,11 @@ describe('Cohort Display', function() {
 
     expect(browser.getTitle()).toEqual('Cohort Pairing');
   });
+
+  it('should have the right names in the html', function() {
+    browser.get('http://localhost:8080');
+
+    expect(element(by.binding('maker.name')).getText()).
+        toEqual('James');
+  });
 });
