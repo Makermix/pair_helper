b51ca81002b30b4567cf07fc6a72afac0025753f
Sara OC <saramoo@hotmail.com>
2015-05-14 10:03:34 +0100

    rollback apiResource to hardcoded object

--- js/cohortController.js
+++ js/cohortController.js
@@ -1,20 +1,20 @@
 makerMix.controller('CohortController', ['$resource', function($resource) {
     var self = this;
-    var apiResource = $resource('http://localhost:9393');
+    // var apiResource = $resource('http://localhost:9393');
 
-    self.cohort = apiResource.get();
-    console.log(self.cohort)
+    // self.cohort = apiResource.get();
+    // console.log(self.cohort)
 
-    // self.cohort = [
-    //     {"id": 1,
-    //     individuals : [
-    //         {
-    //         "name": "James"
-    //         },
-    //         {
-    //         "name": "Rich"
-    //         }
-    //     ]}
-    // ];
+    self.cohort = [
+        {"id": 1,
+        individuals : [
+            {
+            "name": "James"
+            },
+            {
+            "name": "Rich"
+            }
+        ]}
+    ];
 
 }]);
