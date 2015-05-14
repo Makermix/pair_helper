371e988385043b23d127367600d63c2702eb0f32
GJMcGowan <gemcgow@gmail.com>
2015-05-14 16:48:18 +0100

    Angular can paste user input on page...

--- js/userController.js
+++ js/userController.js
@@ -1,3 +1,8 @@
 makerMix.controller('UserController', ['$resource', function($resource) {
     var self = this;
+
+    self.outputName = function() {
+      self.userBox = self.userName
+      console.log(self.userName);
+    };
 }]);
--- test/e2e/userControllerFeature.js
+++ test/e2e/userControllerFeature.js
@@ -6,10 +6,11 @@ describe('User Controller', function() {
 
     var userNameBox = element(by.model('userCtrl.userName'));
     var submitUserButton = element(by.id('woofbtn'));
+    var userReadout = element(by.id('userReadout'));
 
     it('should accept a name from a user', function () {
         userNameBox.sendKeys('James');
         submitUserButton.click();
-        expect(element(by.id('actualUser')).getText).toContain('James');
+        expect(userReadout.getText()).toEqual('James')
     });
 });
