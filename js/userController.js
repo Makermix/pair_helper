c3e9635b603e38876c8151f4b4186b3e3f3c531a
sanjsanj <sanjsanj@hotmail.com>
2015-05-15 14:21:17 +0100

    stuff

--- js/userController.js
+++ js/userController.js
@@ -1,14 +1,27 @@
-makerMix.controller('UserController', ['$resource', function($resource) {
+makerMix.controller('UserController', ['$resource', '$http', function($resource, $http) {
     var self = this;
 
     var URL = 'https://makermix.herokuapp.com/pair/';
-    self.hidden = true
+    self.hidden = true;
+    self.errorHidden = true;
 
     self.outputName = function() {
       self.userName = capitalizeFirstLetter(self.userName)
       var apiResource = $resource(URL + self.userName);
+      $http.get(URL + self.userName).
+        success(function(data, status, headers, config) {
+          // console.log(data.name)
+          self.hidden = false;
+          self.errorHidden = true;
+        }).
+        error(function(data, status, headers, config){
+          // console.log('ERROR OR SOMETHING')
+          self.hidden = true;
+          self.errorHidden = false;
+        });
+
+
       self.userBox = apiResource.get();
-      self.hidden = false;
     };
 
     function capitalizeFirstLetter(string) {
