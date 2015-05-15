1294424c9d900026b2bba49532836913d22b23dd
GJMcGowan <gemcgow@gmail.com>
2015-05-15 11:32:07 +0100

    hiding greeting text test passing

--- js/userController.js
+++ js/userController.js
@@ -2,9 +2,13 @@ makerMix.controller('UserController', ['$resource', function($resource) {
     var self = this;
 
     var URL = 'https://makermix.herokuapp.com/pair/';
+    self.hidden = true
 
     self.outputName = function() {
       var apiResource = $resource(URL + self.userName);
       self.userBox = apiResource.get();
+      self.hidden = false;
+
+
     };
 }]);
