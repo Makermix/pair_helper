4c4b35beb0bea29d91515368a725bedaccc7e632
GJMcGowan <gemcgow@gmail.com>
2015-05-15 12:01:30 +0100

    Adding css/capitalizing inputs

--- js/userController.js
+++ js/userController.js
@@ -5,10 +5,14 @@ makerMix.controller('UserController', ['$resource', function($resource) {
     self.hidden = true
 
     self.outputName = function() {
+      self.userName = capitalizeFirstLetter(self.userName)
       var apiResource = $resource(URL + self.userName);
       self.userBox = apiResource.get();
       self.hidden = false;
-
-
     };
+
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
 }]);
